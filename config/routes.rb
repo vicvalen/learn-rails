4a8012b5dd1bdb3b098595c5cbda820e3e4c0d5a
victor Valencia <vicvalen@gmail.com>
2016-03-09 01:43:23 -0800

    Contact Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :contacts, only: [:new, :create]
+  root to: 'visitors#new'
 
-  # You can have the root of your site routed with "root"
- root to: 'visitors#new'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
