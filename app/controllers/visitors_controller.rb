0e9a35e695fdbf8f8eeef9bcadc5dc13941cf519
victor Valencia <vicvalen@gmail.com>
2016-03-09 00:06:17 -0800

    front end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,6 @@ class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
     render 'visitors/new'
-    flash[:notice] = 'Welcome!'
-    flash[:alert] = 'My birthday is soon.'
 
 
   end
